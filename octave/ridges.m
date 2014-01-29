08fab58fce0c4c59ac35f8f0398c2b6c793eb54d
Mikko Vepsalainen <mtvepsal@gmail.com>
2014-01-29 22:36:26 +0200

    Ridges discovery from phase stability is also kind of working now, but the result seems noisier than the one from maxima.

--- octave/ridges.m
+++ octave/ridges.m
@@ -8,27 +8,47 @@
 ## and nneig defines the number of points on both sides belonging
 ## to the same local neigborhood.
 
-
 function rgm = ridges(sgm, method = "abs", nneig = 2, cutoff = 0.05)
   type = validatestring(method, {"absolute", "phase"});
 
   data  = abs(sgm);
   limit = cutoff * max(data(:));
 
+  % Perhaps we should use octave's findpeaks instead of own hacks?
+
   switch (type)
     case "absolute"
       rgm   = zeros(size(data));
-      % Local maxima
+      % Replace every point by its max neighbor
       for k = -nneig:nneig
         rgm = max(rgm, shift(data, k));
       endfor
+      % Large enough and local maximum
       rgm = (data > limit) .* (rgm == data);
     case "phase"
-      % We still require a clearly nonzero argument
-      idx   = data > limit;
       phase = arg(sgm);
-      % TBC: this seems pretty hard, typically the phase varies so much.
-      % Some kind of unwrapping and smoothing required first?
+      rgm   = ones(size(data));
+      % Plotting the two-sided difference reveals a slow linear trend near the ridges,
+      % which could then be pinpointed at the zero crossings of the linear lines.
+      % Everywhere else the difference oscillates wildly.
+      % (So much that there is not much point doing any unwrapping over the whole column.)
+      dphase = abs(shift(phase, 1, 2) - shift(phase, -1, 2));
+      % It is probably best if we first get rid of most of the oscillations
+      % by only considering high amplitude regions:
+      dphase(find(data < limit)) = nan;
+
+      % Replace every point by its min neighbor
+      for k = -nneig:nneig
+        rgm = min(rgm, shift(dphase, k));
+      endfor
+      % Large enough and local maximum
+      rgm = (data > limit) .* (rgm == dphase);
   endswitch
 
+endfunction
+
+
+
+function phase = uwphase(sgm)
+  
 endfunction
