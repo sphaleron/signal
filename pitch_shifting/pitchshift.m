4b76ff541f47891dd47df3ddbd885723813b2d26
Mikko Vepsalainen <mtvepsal@gmail.com>
2014-02-08 19:55:35 +0200

    Kind of working, but have to understand exactly what Octave stft/synthesis calculates to get back a signal of same length (should probably pad with zeros or something).

--- pitch_shifting/pitchshift.m
+++ pitch_shifting/pitchshift.m
@@ -6,19 +6,41 @@
 ## and stepping the window N/4 points between iterations.
 ##
 ## Note: this handles now the whole signal at once. Real-time implementation
-## should be quite different.
+## should work differently.
 
+## At sampling frequency Fs the time between successive samples is 1/Fs,
+## and the time difference between frames is ta = inc / Fs. As a first step
+## we want to scale the length of the frame (and interval between frames) so
+## that the interval becomes ts = s*ta
+## When using window (and DFT) length N, the true angular frequency of bin 'k'
+## is 2*pi*k*Fs (for k < N/2, the rest should be interpreted as negative frequncies,
+## but that does not matter here).
 
 function output = pitchshift(signal, steps, N)
   inc = floor(N/4);
   s   = 2**(steps/12);
-  [sgm, pars] = stft(signal, N, inc, N, 2);
+#   signal = 
+  [sgm, pars] = stft(signal, N, inc, N/2, 2);
   power = abs(sgm);
-  % Unwrap the phase to get correct phase shifts
-  phase = unwrap(arg(sgm), pi, 2);
-  % imagesc(power)
-  % imagesc(phase)
+  % Wrapped phase
+  phase = arg(sgm);
+  % Add a column of zeros before taking the differences.
+  phase = prepad(phase, size(phase, 2) + 1, 0, 2);
+  % Phase difference between successive frames
   dphase = phase - shift(phase, 1, 2);
-  % imagesc(abs(dphase))
-  
+  dphase = dphase(:, 2:end);
+
+  % Subtract the "natural" phase difference due to bin center frequency (not necessary?)
+  dphase -= 2*pi*inc*(0:N-1)';
+  % Normalize the phase and restore the natural component
+  dphase = fmod(dphase + pi, 2*pi) - pi;
+  dphase += 2*pi*inc*(0:N-1)';
+  % During time ts the phase should then increase by w*ts:
+  dphase = cumsum(s * dphase, 2);
+  sgm = power .* exp(i*dphase);
+
+  % Inverse windowed DFT
+  output = synthesis(sgm, pars);
+
+
 endfunction
